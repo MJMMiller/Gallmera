8820617f5584600f779c16546efaabfaf5e51d3c
MJMMiller <mateomiller1605@gmail.com>
2024-05-10 08:09:16 -0500

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,21 @@ export class HomePage {
 
   constructor() {}
 
+  ngOnInit(): void {
+    Camera.requestPermissions();
+  }
+
+  imagenParaMostrar: string = "";
+  imagenTomada: any;  
+
+  async getPicture() {
+    this.imagenTomada = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+    if (this.imagenTomada) {
+      this.imagenParaMostrar = this.imagenTomada;
+    }
+  }  
 }
